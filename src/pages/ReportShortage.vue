59c215f8efec24cc643be19e6975d46a5197fba9
David Giardi <david.giardi.eng@gmail.com>
2020-03-30 02:47:45 +0200

    Add food images

--- src/pages/ReportShortage.vue
+++ src/pages/ReportShortage.vue
@@ -36,9 +36,10 @@
         <div v-ripple class="cursor-pointer relative-position card-ripple" @click="activateItem(item, k)">
 
         <q-img
-          src="https://cdn.quasar.dev/img/parallax2.jpg"
+          :src="item.name in imageTable ? imageTable[item.name] : imageDefault"
           basic
           class="card-image"
+          :ratio="1"
         >
           <div class="absolute-bottom card-image-cap text-subtitle2 text-center">
             {{item.name}}
@@ -108,17 +109,65 @@ export default {
       currentBrowseList: [],
       browseStack: [],
 
+      imageDefault: 'statics/itempics/nathalia-rosa-rWMIbqmOxrY-unsplash.jpg',
+      imageTable: {
+        'Apples' : "statics/itempics/matheus-cenali-wXuzS9xR49M-unsplash.jpg",
+        'Pasta' : "statics/itempics/chuttersnap-ycnyotoyk8o-unsplash.jpg",
+        'Pharmacy' : "statics/itempics/enecta-cannabis-extracts-8yF6aQ3NL7w-unsplash.jpg",
+        'Toilet paper' : "statics/itempics/erik-mclean-GNHkPsONmac-unsplash.jpg",
+        'Coffee' : "statics/itempics/estela-shaddix-kzkH7Vh11fs-unsplash.jpg",
+        'Carrots' : "statics/itempics/harshal-s-hirve-yNB8niq1qCk-unsplash.jpg",
+        'Produce' : "statics/itempics/ja-ma--gOUx23DNks-unsplash.jpg",
+        'Pears' : "statics/itempics/jonathan-mast-RW6Wz9QaoKk-unsplash.jpg",
+        'Cooking oil' : "statics/itempics/jonathan-ocampo-iCgfwfqgdzo-unsplash.jpg",
+        'Flour' : "statics/itempics/kristiana-pinne-FL8NSCfvty0-unsplash.jpg",
+        'Cat food' : "statics/itempics/ramiz-dedakovic-9SWHIgu8A8k-unsplash.jpg",
+        'Eggs' : "statics/itempics/rebekah-howell-A4z0UBgAwyc-unsplash.jpg",
+        'Soap' : "statics/itempics/ruby-tGSr_dzVBZw-unsplash.jpg",
+        'Pet supplies' : "statics/itempics/tran-mau-tri-tam-7QjU_u2vGDs-unsplash.jpg",
+        'Dog food' : "statics/itempics/victor-grabarczyk-N04FIfHhv_k-unsplash.jpg",
+        'Sponge' : "statics/itempics/volodymyr-hryshchenko-WU9dA3C4R28-unsplash.jpg",
+        'Cat litter' : "statics/itempics/wolfgang-hasselmann-VH8QlHCkZ-U-unsplash.jpg",
+        'Masks' : "statics/itempics/de-an-sun-gki8YD94Qg0-unsplash.jpg",
+        'Disinfectant' : "statics/itempics/enecta-cannabis-extracts-8yF6aQ3NL7w-unsplash.jpg",
+        'Milk' : "statics/itempics/gabi-miranda-dxb_HSjoQ40-unsplash.jpg",
+        'Bread' : "statics/itempics/wesual-click-rsWZ-P9FbQ4-unsplash.jpg",
+        'Sugar' : "statics/itempics/sharon-mccutcheon-oKay0q7Pa30-unsplash.jpg",
+        'House supplies' : "statics/itempics/scott-webb-1ddol8rgUH8-unsplash.jpg",
+        'Rice': "statics/itempics/pille-riin-priske-xmuIgjuQG0M-unsplash.jpg",
+        'Cheese': "statics/itempics/waldemar-brandt-kPqaqug998Y-unsplash.jpg",
+        'Chocolate': "statics/itempics/charisse-kenion-tWe8ib-cnXY-unsplash.jpg",
+        'Biscuits': "statics/itempics/christina-branco-7P-wc2Z2Ujs-unsplash.jpg",
+        'Frozen pizza': "statics/itempics/alan-hardman-SU1LFoeEUkk-unsplash.jpg",
+        'Tomato sauce': "statics/itempics/sara-dubler-SdxnUyHEKGY-unsplash.jpg",
+        'Canned food': "statics/itempics/calle-macarone-Vl78eNdiJaQ-unsplash.jpg",
+        'Food': "statics/itempics/angelo-pantazis-1uIy-v4OlIk-unsplash.jpg",
+        'Hygiene': "statics/itempics/curology-29wToZsX0Is-unsplash.jpg",
+        'Lettuce': "statics/itempics/agence-producteurs-locaux-damien-kuhn-fd05H8aHoXY-unsplash.jpg",
+        'Lemons': "statics/itempics/bannon-morrissy-DlIf_0GR_nM-unsplash.jpg",
+        'Oranges': "statics/itempics/bannon-morrissy-kgkhD_kPSzQ-unsplash.jpg",
+        'Potatoes': "statics/itempics/lars-blankers-B0s3Xndk6tw-unsplash.jpg",
+        'Onions': "statics/itempics/lars-blankers-pmS8XSz5NU0-unsplash.jpg",
+        'Peppers': "statics/itempics/martin-adams-_LGlGi3KJIA-unsplash.jpg",
+        'Printer ink': "statics/itempics/museums-victoria-bat9Y6pSoTI-unsplash.jpg",
+        'Grapes': "statics/itempics/rajesh-rajput-y2MeW00BdBo-unsplash.jpg",
+        'Bananas': "statics/itempics/waldemar-brandt-kV3GEmrSp8k-unsplash.jpg",
+        'Pen': "statics/itempics/steve-johnson-htqCupyYlCQ-unsplash.jpg",
+        'Light bulb': "statics/itempics/federico-bottos-TuAtSs8peoM-unsplash.jpg",
+        'Paper': "statics/itempics/brandi-redd-aJTiW00qqtI-unsplash.jpg",
+        'Tomatoes': "statics/itempics/lars-blankers-6Z7Ss9jlEL0-unsplash.jpg",
+      },
+
       listItems: [
         {name: 'Food', subsections: 
           [
             {name:'Produce', subsections: 
               [
                 {name:'Tomatoes'},
-                {name:'Zucchini'},
                 {name:'Onions'},
                 {name:'Carrots'},
                 {name:'Lettuce'},
-                {name:'Potateos'},
+                {name:'Potatoes'},
                 {name:'Peppers'},
                 {name:'Apples'},
                 {name:'Oranges'},
@@ -129,7 +178,7 @@ export default {
               ]
             },
             {name:'Bread'},
-            {name:'Butter'},
+            {name:'Eggs'},
             {name:'Milk'},
             {name:'Pasta'},
             {name:'Rice'},
@@ -143,19 +192,13 @@ export default {
             {name:'Coffee'},
             {name:'Canned food'},
             {name:'Frozen pizza'},
-            {name:'Frozen vegetables'},
           ] 
         },
         {name: 'Hygiene', subsections: 
           [
             {name:'Toilet paper'},
-            {name:'Multi-use paper'},
             {name:'Sponge'},
-            {name:'Dish soap'},
             {name:'Soap'},
-            {name:'Shampoo'},
-            {name:'Deodorant'},
-            {name:'Laundry detergent'},
           ] 
         },
         {name: 'House supplies', subsections: 
@@ -163,9 +206,13 @@ export default {
             {name:'Light bulb'},
             {name:'Paper'},
             {name:'Pen'},
-            {name:'Pencil'},
             {name:'Printer ink'},
-            {name:'Kitchenware'},
+          ] 
+        },        
+        {name: 'Pharmacy', subsections: 
+          [
+            {name:'Masks'},
+            {name:'Disinfectant'},
           ] 
         },
         {name: 'Pet supplies', subsections: 
